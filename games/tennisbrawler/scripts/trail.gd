57c8dcd0d6c949881891fc31d1e1330de60e01c8
HippopoStar <leocabanes@wanadoo.fr>
2025-05-30 16:43:45 +0200

    TennisBrawler: remove unused parameter

--- games/tennisbrawler/scripts/trail.gd
+++ games/tennisbrawler/scripts/trail.gd
@@ -23,13 +23,12 @@ func update_alpha() -> void:
 		var segment_color = segment.get_node("MeshInstance3D").get_mesh().get_material().get_albedo()
 		segment.get_node("MeshInstance3D").get_mesh().get_material().set_albedo(segment_color - Color(0, 0, 0, self.alpha_step))
 
-func update_segment(delta: float, segment: Node3D) -> void:
+func update_segment(segment: Node3D) -> void:
 	var deplacement: Vector3 = self.global_position - self.previous_global_position
 	if deplacement.is_zero_approx():
 		segment.set_visible(false)
 	else:
 		segment.set_visible(true)
-		#var cylinder_mesh_height: float = (self.get_parent().get_linear_velocity() * delta).length()
 		var cylinder_mesh_height: float = deplacement.length()
 		# MeshInstance3D.get_mesh() -> Mesh
 		# CylinderMesh.set_height(value: float) -> void
@@ -62,4 +61,4 @@ func _process(delta: float) -> void:
 		segment = self.segments_array.get(self.index)
 		self.index = (self.index + 1) % self.GRANULARITY
 	self.update_alpha()
-	self.update_segment(delta, segment)
+	self.update_segment(segment)
